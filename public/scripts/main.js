2a06b881f71e7c22df3816b4e2a1d41344dc2831
ashayp22 <ashayparikh22@gmail.com>
2020-03-26 14:37:22 -0500

    done with cookies

---
+++ public/scripts/main.js
@@ -0,0 +1,20 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+//check if a user is in session
+if(getCookie("user") != "yes" && window.location.pathname != "/data") {
+  window.location = "/data" //redirect to data
+}
