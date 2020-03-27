6d23d1c7145d4bd7aa804573ec7f1a017d8c07ec
ashayp22 <ashayparikh22@gmail.com>
2020-03-26 20:58:34 -0500

    found hand model

--- public/scripts/camera.js
+++ public/scripts/camera.js
@@ -1,33 +1,53 @@
 // Load the model.
+
+var handModel;
+
   handTrack.load().then(model => {
+    handModel = model
     const img = document.getElementById('img');
     alert("loaded")
 
+    handModel.detect(img).then(predictions => {
+      console.log('Predictions: ', predictions);
+    });
+
     // Grab elements, create settings, etc.
-    var video;
+    var video = document.getElementById('video');
 
-    function setup() {
-      video = document.getElementById('video');
-      // Elements for taking the snapshot
-
-      // Get access to the camera!
-      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          // Not adding `{ audio: true }` since we only want video now
-          navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-              //video.src = window.URL.createObjectURL(stream);
-              video.srcObject = stream;
-              video.play();
-          });
-      }
-
+    // Get access to the camera!
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+          //video.src = window.URL.createObjectURL(stream);
+          video.srcObject = stream;
+          video.play();
+          animate()
+      });
     }
 
-    // // detect objects in the image.
-    // model.detect(img).then(predictions => {
+  });
+
+  function animate() {
+    drawHands()
+    predictHands()
+    a = requestAnimationFrame(animate);
+  }
+
+  function drawHands() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var video = document.getElementById('video');
+    ctx.drawImage(video, 0, 0, 640, 480);
+  }
+
+  function predictHands() {
+
+    // var img = new Image();
+    // var canvas = document.getElementById('canvas');
+    // img.src = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
+    // img.width = 640;
+    // img.height = 480;
+    //
+    // handModel.detect(img).then(predictions => {
     //   console.log('Predictions: ', predictions);
     // });
-
-
-
-
-  });
+  }
