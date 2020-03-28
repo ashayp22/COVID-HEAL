02509a73836ca652125c4a20f01d7ca2bc36fcb8
sohumbhole <48001832+sohumbhole@users.noreply.github.com>
2020-03-28 12:13:22 -0500

    Add files via upload
    uploaded memes. Meme have a top text and bottom text. Calling the loadMemes function populates the three arrays that hold the text and image data.

---
+++ public/scripts/media.js
@@ -0,0 +1,28 @@
+var image = [];
+var toptext = [];
+var bottomText = [];
+function loadQuote() {
+
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
+        alert(data.quoteText);
+        alert(data.quoteAuthor);
+        alert(data.quoteLink);
+    })
+}
+
+function loadMemes() {
+    $.getJSON("http://alpha-meme-maker.herokuapp.com/submissions", function(data) {
+        for (var i = 0; i < data.length; i++) {
+            toptext.push(data[i].topText);
+            bottomText.push(data[i].bottomText);
+            loadMemeImage(data[i].memeID);
+        }
+    })
+}
+
+function loadMemeImage(id) {
+
+    $.getJSON("http://alpha-meme-maker.herokuapp.com/memes/" + id + "/", function(data) {
+        image.push(data.image);
+    })
+}
