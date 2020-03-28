8c26dc13cc4718769f891b30d3fd243c12d71d90
Labdhi Jain <labdhijain753@gmail.com>
2020-03-28 08:58:47 -0500

    Corona Stats
    Signed-off-by: Labdhi Jain <labdhijain753@gmail.com>

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,6 +4,34 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+
+
+$.ajax({
+    type: "GET",
+    url: "https://api.smartable.ai/coronavirus/stats/global",
+
+    // Request headers
+    beforeSend: function (xhrObj) {
+        xhrObj.setRequestHeader("Cache-Control", "no-cache");
+        xhrObj.setRequestHeader("Subscription-Key", "eba31584641f4cbeae6f39397823b9a1");
+    },
+})
+    .done(function (data) {
+
+        document.getElementById("total_cases").innerHTML = data.totalConfirmedCases
+        document.getElementById("total_deaths").innerHTML = data.totalDeaths
+        document.getElementById("total_recoveries").innerHTML = data.totalRecoveredCases
+
+
+        alert("success");
+    })
+    .fail(function () {
+        alert("error");
+    });
+
+
+
 !(function($) {
   "use strict";
 
@@ -182,5 +210,6 @@
   });
 
 })(jQuery);
+
 
 
