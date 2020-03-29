51961410ed60c383221b02cbd4d1870770e99bf3
ashayp22 <ashayparikh22@gmail.com>
2020-03-29 13:18:05 -0500

    a

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -54,10 +54,12 @@ function callAPI(state) {
               var div3 = document.createElement('div');
               div3.className = "card-body"
 
-              console.log(news[i].images != null)
-
               if(news[i].images != null) {
                 div3.style.backgroundImage = "url(" + news[i].images[0].url + ")";
+                var img = document.createElement("img");
+                img.src = news[i].images[0].url
+                console.log(img.src)
+                console.log(getAverageRGB(img))
               } else {
                 div3.style.backgroundImage = "url(resources/covid.jpg)";
               }
@@ -103,6 +105,52 @@ function callAPI(state) {
           header.style.textAlign = "center"
           document.getElementById("features").appendChild(header);
         });
+
+}
+
+function getAverageRGB(imgEl) {
+
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+
+    if (!context) {
+        return defaultRGB;
+    }
+
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+    context.drawImage(imgEl, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */
+        return defaultRGB;
+    }
+
+    length = data.data.length;
+
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+
+    return rgb;
 
 }
 
