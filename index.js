eab4a2a532c606a42a8d534412ef6130d97f2831
ashayp22 <ashayparikh22@gmail.com>
2020-10-03 13:28:01 -0500

    cors issue

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "covidheal.org"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.post('/alert', (req, res) => {
   const subscription = req.body;
